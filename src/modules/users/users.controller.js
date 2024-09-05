042f897972da3c861e08091baf67a1c31b90e239
Islam Ashraf <eslam.01212@gmail.com>
2024-09-05 17:30:33 +0300

    refactor: renaming variables

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -6,18 +6,18 @@ export async function getUser(req, res, _next) {
     const { userId } = req.params;
 
     try {
-        const user = await getOrSet(
+        const userWithoutPassword = await getOrSet(
             `user:${userId}`,
             async () => {
                 const userRow = await User.findByPk(userId, { attributes: { exclude: ['password'] } });
-                if (!userRow) throw new Error;
-                return user.dataValues;
+                return userRow?.dataValues;
             }
         );
+        if (!userWithoutPassword) {
+            return res.status(404).json({ message: 'User not found' });
+        }
 
-        if (!user) return res.status(404).json({ message: 'User not found' });
-
-        res.status(200).json({ user });
+        res.status(200).json({ user: userWithoutPassword });
     } catch {
         throw new Error;
     }
@@ -27,10 +27,10 @@ export async function updateUser(req, res, _next) {
     const { userId } = req.params;
     const { body: userData } = req;
 
-    const user = await userService.updateUser(userId, userData);
-    if (!user) {
+    const userWithoutPassword = await userService.updateUser(userId, userData);
+    if (!userWithoutPassword) {
         return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json({ user });
+    res.status(200).json({ user: userWithoutPassword });
 }
