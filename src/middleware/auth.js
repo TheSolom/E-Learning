b49c1fc7595c42f0f751b4910f4f8df50756676c
Islam Ashraf <eslam.01212@gmail.com>
2024-09-06 15:23:51 +0300

    feat: implement user roles

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,7 @@
 import { verifyToken } from '../modules/auth/token.service.js';
 import redis from "../config/redis.js";
 import errorHandler from '../utils/error-handler.js';
+import roles from '../modules/users/roles.enum.js';
 
 export async function isAuthenticated(req, _res, next) {
     const accessToken = req.cookies["accessToken"] ?? req.headers["authorization"]?.split(' ')[1];
@@ -28,3 +29,21 @@ export function authorizeRoles(...rolesIds) {
         next();
     };
 };
+
+export function isSameUserOrAdmin(req, _res, next) {
+    const { userId } = req.params;
+    if (!userId) {
+        throw new errorHandler('User ID is required', 400);
+    }
+    if (
+        userId !== req?.user.id &&
+        (
+            req.user.roleId !== roles.ADMIN ||
+            req.user.roleId === roles.SUPER_ADMIN
+        )
+    ) {
+        throw new errorHandler('Access restricted, insufficient permissions', 403);
+    }
+
+    next();
+}
---
+++ src/modules/users/roles.enum.js
@@ -0,0 +1,9 @@
+const roles = Object.freeze({
+    STUDENT: 1,
+    CO_INSTRUCTOR: 2,
+    INSTRUCTOR: 3,
+    ADMIN: 4,
+    SUPER_ADMIN: 5,
+});
+
+export default roles;
