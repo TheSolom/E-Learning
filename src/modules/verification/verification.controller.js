960b72371b3be5f951335dfa9bdc57ae6c08c5d4
Islam Ashraf <eslam.01212@gmail.com>
2024-09-19 22:39:43 +0300

    refactor: improved error handling
    - Simplified OTP sending by directly fetching user details in `sendOTP`
    - Removed unnecessary cool-down checks
    - Replaced `updateUser` with `getUser` to ensure consistency in user retrieval

--- src/modules/verification/verification.controller.js
+++ src/modules/verification/verification.controller.js
@@ -1,32 +1,21 @@
 import { OTPFactory } from './otp.factory.js';
 import { EmailStrategyContext } from '../sender/email.strategy.js';
 import { VerificationPurpose } from './verification.enum.js';
-import errorHandler from '../../utils/error-handler.js';
-import { updateUser } from '../users/users.service.js';
+import { getUser, updateUser } from '../users/users.service.js';
 
 export async function sendOTP(req, res, _next) {
-    const { userId, email, purpose } = req.body;
+    const { body: { userId, purpose } } = req;
+
+    const user = await getUser(userId);
 
     const otpHandler = OTPFactory.getOTPPurposeHandler(purpose);
 
-    const { isCoolDown, remainingTime } = await otpHandler.checkCoolDown(userId);
-    if (isCoolDown) {
-        return res.status(429).json({
-            message: `Please wait ${remainingTime} before requesting a new OTP`,
-        });
-    }
+    await otpHandler.checkCoolDown(userId);
 
-    try {
-        const createdOTP = await otpHandler.createOTP(userId);
+    const createdOTP = await otpHandler.createOTP(userId);
 
-        const emailStrategy = new EmailStrategyContext(purpose);
-        await emailStrategy.sendEmail(email, createdOTP);
-
-    } catch (error) {
-        if (error?.original.code === '23500') {
-            throw new errorHandler('Invalid user id', 409);
-        }
-    }
+    const emailStrategy = new EmailStrategyContext(purpose);
+    await emailStrategy.sendEmail(user.email, createdOTP);
 
     res.status(201).json({
         message: `OTP sent, please check your email and verify it`,
@@ -38,19 +27,17 @@ export async function sendOTP(req, res, _next) {
 }
 
 export async function verifyOTP(req, res, _next) {
-    const { userId, otp, purpose } = req.body;
+    const { body: { userId, purpose, otp } } = req;
 
     const otpHandler = OTPFactory.getOTPPurposeHandler(purpose);
 
-    const { isValid, cause } = await otpHandler.verifyOTP(userId, otp);
-    if (!isValid) {
-        throw new errorHandler(cause, 400);
-    }
+    await otpHandler.verifyOTP(userId, otp);
 
-    await otpHandler.removeOTP(userId, otp);
-
-    if (purpose === VerificationPurpose.EMAIL_VERIFICATION)
-        await updateUser(userId, { isVerified: true });
+    await Promise.all([
+        otpHandler.removeOTP(userId, otp),
+        purpose === VerificationPurpose.EMAIL_VERIFICATION ?
+            updateUser(userId, { isVerified: true }) : Promise.resolve()
+    ]);
 
     res.status(200).json({ message: 'OTP verified successfully' });
 }
