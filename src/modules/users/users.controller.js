e2639009185937996a5b3d9606671d0acfb93a92
Islam Ashraf <eslam.01212@gmail.com>
2024-09-19 22:54:02 +0300

    refactor: user controllers and services
    - Simplified parameter extraction for `getUser` and `updateUser` controllers
    - Updated `deleteUser` to use headers for password validation
    - Enhanced `getUser` service to include associated roles
    - Added `getUsers` service
    - Implemented `checkUserPassword` service for password validation
    - Simplified `deleteUser` service logic

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -1,36 +1,38 @@
 import * as userService from './users.service.js';
 
 export async function getUser(req, res, _next) {
-    const { userId } = req.params;
+    const { params: { userId } } = req;
 
     const userWithoutPassword = await userService.getUser(userId);
-    if (!userWithoutPassword) {
-        return res.status(404).json({ message: 'User not found' });
-    }
 
     res.status(200).json({ user: userWithoutPassword });
 }
 
+export async function getUsers(req, res, _next) {
+    const { query: findOptions } = req;
+
+    const usersData = await userService.getUsers(findOptions);
+
+    res.status(200).json(usersData);
+}
+
 export async function updateUser(req, res, _next) {
-    const { userId } = req.params;
+    const { params: { userId } } = req;
     const { body: userData } = req;
 
     const userWithoutPassword = await userService.updateUser(userId, userData);
-    if (!userWithoutPassword) {
-        return res.status(404).json({ message: 'User not found' });
-    }
 
     res.status(200).json({ user: userWithoutPassword });
 }
 
 export async function deleteUser(req, res, _next) {
-    const { userId: deletedUserId } = req.params;
-    const { body: { userId, password } } = req;
+    const { user: { id: requestingUserId } } = req;
+    const { headers: { 'x-password': requestingUserPassword } } = req;
+    const { params: { userId } } = req;
 
-    const isDeleted = await userService.deleteUser(userId, password, deletedUserId);
-    if (!isDeleted) {
-        return res.status(400).json({ message: 'Password is incorrect' });
-    }
+    await userService.checkUserPassword(requestingUserId, requestingUserPassword);
+
+    await userService.deleteUser(userId);
 
     res.status(204).json();
 }
--- src/modules/users/users.service.js
+++ src/modules/users/users.service.js
@@ -1,50 +1,131 @@
+import { Op } from 'sequelize';
 import User from './users.model.js';
 import { getOrSet, set, remove } from '../../utils/cache.js';
+import ErrorHandler from '../../utils/error.handler.js';
 
 export const getUser = async (identifier) => {
-    if (typeof identifier === 'string' || typeof identifier === 'number') {
-        const userWithoutPassword = await getOrSet(
-            `user:${identifier}`,
-            async () => {
-                const userRow = await User.findByPk(identifier, { attributes: { exclude: ['password'] } });
-                return userRow?.dataValues;
-            }
-        );
+    let user = null;
 
-        return userWithoutPassword;
+    if (typeof identifier === 'string' || typeof identifier === 'number') {
+        user = await getOrSet(`user:${identifier}`,
+            async () => {
+                return (await User.findByPk(identifier, {
+                    attributes: { exclude: ['password'] },
+                    include: [{
+                        model: User.sequelize.models.role,
+                        attributes: ['id', 'sortOrder'],
+                        through: {
+                            model: User.sequelize.models.user_role,
+                            attributes: []
+                        },
+                    }]
+                }))?.dataValues;
+            });
     } else if (typeof identifier === 'object') {
-        const userWithoutPassword = await User.findOne({ where: identifier, attributes: { exclude: ['password'] } });
-        return userWithoutPassword?.dataValues;
+        user = await User.findOne({
+            where: identifier,
+            include: [{
+                model: User.sequelize.models.role,
+                attributes: ['id', 'sortOrder'],
+                through: {
+                    model: User.sequelize.models.user_role,
+                    attributes: []
+                },
+            }]
+        });
     }
 
-    return null;
+    if (!user) {
+        throw new ErrorHandler('User not found', 404);
+    }
+
+    return user;
+};
+
+export const getUsers = async (findOptions) => {
+    const { page, limit, search, roleId, languageId, order, sort } = findOptions;
+
+    const includeOptions = [{
+        model: User.sequelize.models.role,
+        required: true,
+        attributes: ['id', 'name', 'sortOrder'],
+        through: {
+            model: User.sequelize.models.user_role,
+            attributes: [],
+            ...(roleId && { where: { roleId } }),
+        },
+    }, {
+        model: User.sequelize.models.language,
+        attributes: ['id', 'name'],
+        through: {
+            model: User.sequelize.models.user_language,
+            attributes: [],
+            ...(languageId && { where: { languageId } }),
+        }
+    }];
+
+    const offset = (page - 1) * limit;
+
+    const { rows: users, count: total } = await User.findAndCountAll({
+        where: {
+            ...(search && {
+                [Op.or]: [
+                    {
+                        firstName: {
+                            [Op.like]: `%${search}%`,
+                        },
+                    },
+                    {
+                        lastName: {
+                            [Op.like]: `%${search}%`,
+                        },
+                    },
+                    {
+                        email: {
+                            [Op.like]: `%${search}%`,
+                        },
+                    },
+                ],
+            }),
+        },
+        attributes: { exclude: ['password'] },
+        include: includeOptions,
+        order: [[sort, order]],
+        limit,
+        offset,
+    });
+
+    return {
+        total,
+        currentPage: page,
+        totalPages: Math.ceil(total / limit),
+        data: users.map((user) => user.dataValues),
+    };
 };
 
 export const updateUser = async (userId, userData) => {
     const user = await User.findByPk(userId);
-    if (!user) return null;
+    if (!user) {
+        throw new ErrorHandler('User not found', 404);
+    }
 
-    const { dataValues: { password, ...userWithoutPassword } } = await user.update(userData);
+    const { dataValues: { password: _, ...userWithoutPassword } } = await user.update(userData);
 
     await set(`user:${userId}`, userWithoutPassword);
 
     return userWithoutPassword;
 };
 
-export const deleteUser = async (userId, userPassword, deletedUserId) => {
+export const checkUserPassword = async (userId, userPassword) => {
     const user = await User.findByPk(userId);
-    if (!user) return false;
 
-    const isPasswordValid = await user.comparePassword(userPassword);
-    if (!isPasswordValid) return false;
-
-    if (userId === deletedUserId) {
-        await user.destroy();
-    } else {
-        await User.destroy({ where: { id: deletedUserId } });
+    if (!user || !(await user.comparePassword(userPassword))) {
+        throw new ErrorHandler('Invalid password', 401);
     }
+};
+
+export const deleteUser = async (deletedUserId) => {
+    await User.destroy({ where: { id: deletedUserId } });
 
     await remove(`user:${deletedUserId}`);
-
-    return true;
 };
