fb603497ee17c193e41bdffe8f49ba3ce939a420
Islam Ashraf <eslam.01212@gmail.com>
2024-09-19 22:36:37 +0300

    refactor: authentication flow for enhanced token management and error handling
    - Replaced `findUser` with `getUser`
    - improved error handling
    - Removed redundant user details in `register` and `forgotPassword` functions
    - Improved role and verification handling across authentication functions

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,72 +1,75 @@
 import * as authService from "./auth.service.js";
 import { createLoginTokens, validateRefreshToken, verifyToken, blockToken } from './token.service.js';
-import { findUser, updateUser } from '../users/users.service.js';
+import { getUser, updateUser } from '../users/users.service.js';
+import ErrorHandler from "../../utils/error.handler.js";
 import { set, remove } from '../../utils/cache.js';
 import { VerificationPurpose } from '../verification/verification.enum.js';
-import { sendOTP } from '../verification/verification.controller.js';
 
-export async function register(req, res, next) {
+export async function register(req, _res, next) {
     const { body: userData } = req;
 
-    const { id: userId, email } = await authService.registerUser(userData);
+    const { id: userId } = await authService.registerUser(userData);
 
-    req.body = { userId, email, purpose: VerificationPurpose.EMAIL_VERIFICATION };
-    await sendOTP(req, res, next);
+    req.body = {
+        userId,
+        purpose: VerificationPurpose.EMAIL_VERIFICATION
+    };
+
+    next();
 }
 
 export async function login(req, res, next) {
     const { body: userCredentials } = req;
 
-    const userWithoutPassword = await authService.loginUser(userCredentials);
+    const userWithoutPassword = await authService.checkUserCredentials(userCredentials);
 
     if (!userWithoutPassword.isVerified) {
         return res.status(403).json({
             message: "User not verified. please verify your email",
             user: {
                 id: userWithoutPassword.id,
-                email: userWithoutPassword.email
             }
         });
     }
 
-    const { id, email, isVerified, roleId } = userWithoutPassword;
+    const { id, email, isVerified, roles } = userWithoutPassword;
 
-    const [{ accessToken: newAccessToken, refreshToken: newRefreshToken }] = await Promise.all([
-        createLoginTokens(
-            {
-                user: {
-                    id,
-                    email,
-                    isVerified,
-                    roleId
+    const [{
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken
+    }] =
+        await Promise.all([
+            createLoginTokens(
+                {
+                    user: {
+                        id,
+                        email,
+                        isVerified,
+                        roles,
+                    },
+                    isSecure: true
                 },
-                isSecure: true
-            },
-        ),
-        set(`user:${userWithoutPassword.id}`, userWithoutPassword),
-    ]);
+            ),
+            set(`user:${userWithoutPassword.id}`, userWithoutPassword),
+        ]);
 
-    req.accessToken = newAccessToken;
-    req.refreshToken = newRefreshToken;
+    req.body = {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken
+    };
 
     next();
 }
 
-export async function refreshLogin(req, res, next) {
+export async function refreshLogin(req, _res, next) {
     const { refreshToken } = req.cookies ?? req.body;
     if (!refreshToken) {
-        return res.status(401).json({
-            message: 'Token invalid, please login again',
-        });
+        throw new ErrorHandler('Refresh token is required', 400);
     }
 
     const decodedToken = await validateRefreshToken(refreshToken);
-    if (!decodedToken)
-        return res.status(401).json({
-            message: 'Token invalid, please login again',
-        });
 
-    const { id, email, isVerified, roleId } = decodedToken.user;
+    const { id, email, isVerified, roles } = decodedToken.user;
 
     const {
         accessToken: newAccessToken,
@@ -77,14 +80,16 @@ export async function refreshLogin(req, res, next) {
                 id,
                 email,
                 isVerified,
-                roleId
+                roles,
             },
             isSecure: false,
         },
     );
 
-    req.accessToken = newAccessToken;
-    req.refreshToken = newRefreshToken;
+    req.body = {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken
+    };
 
     next();
 }
@@ -92,60 +97,48 @@ export async function refreshLogin(req, res, next) {
 export async function logout(req, res, _next) {
     const { refreshToken } = req.cookies ?? req.body;
     if (!refreshToken) {
-        return res.status(401).json({
-            message: 'Token invalid',
-        });
+        throw new ErrorHandler('Refresh token is required', 400);
     }
 
     const decodedToken = verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-    if (!decodedToken) {
-        return res.status(401).json({
-            message: 'Token invalid',
-        });
-    }
 
     await Promise.all([
         blockToken(refreshToken),
         remove(`user:${decodedToken.user.id}`)
     ]);
 
-    const cookieOption = {
+    const cookieOptions = {
         httpOnly: true,
         sameSite: "lax",
         secure: (process.env.NODE_ENV === "production" ? true : false),
     };
 
     res
-        .clearCookie('refreshToken', cookieOption)
-        .clearCookie('accessToken', cookieOption)
+        .clearCookie('refreshToken', cookieOptions)
+        .clearCookie('accessToken', cookieOptions)
         .status(200).json({
             message: 'Logged out successfully'
         });
 }
 
-export async function forgotPassword(req, res, next) {
-    const { email } = req.body;
+export async function forgotPassword(req, _res, next) {
+    const { body: { email } } = req;
 
-    const user = await findUser({ email });
-    if (!user) {
-        return res.status(404).json({
-            message: 'User not found'
-        });
-    }
+    const user = await getUser({ email });
 
-    req.body = { userId: user.id, email, purpose: VerificationPurpose.PASSWORD_RESET };
-    await sendOTP(req, res, next);
+    req.body = {
+        userId: user.id,
+        email,
+        purpose: VerificationPurpose.PASSWORD_RESET
+    };
+
+    next();
 }
 
 export async function resetPassword(req, res, _next) {
-    const { userId, password } = req.body;
+    const { body: { userId, password } } = req;
 
-    const userWithoutPassword = await updateUser(userId, password);
-    if (!userWithoutPassword) {
-        return res.status(404).json({
-            message: 'User not found'
-        });
-    }
+    await updateUser(userId, password);
 
     res.status(200).json({
         message: 'Password reset successfully'
