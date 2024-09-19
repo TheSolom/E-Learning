9e5956f04a89dbc35a708bcfb0a97f8e23f6ae38
Islam Ashraf <eslam.01212@gmail.com>
2024-09-19 21:10:40 +0300

    refactor: change file name

--- src/utils/error.handler.js
+++ src/utils/error.handler.js
@@ -1,17 +1,17 @@
-class errorHandler extends Error {
+class ErrorHandler extends Error {
     constructor(message, statusCode, cause) {
         super(message);
 
         this.name = this.constructor.name;
         this.statusCode = statusCode || 500;
+        this.isOperational = true;
         if (cause) {
             if (!Array.isArray(cause)) cause = [cause];
             this.cause = cause;
         }
-        this.isOperational = true;
 
         Error.captureStackTrace(this, this.constructor);
     }
 }
 
-export default errorHandler;
+export default ErrorHandler;
